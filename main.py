3bfec850d0786c9cf80e9570430639af0d586815
Niklas Bliemel <niklas.bliemel@stud.uni-regensburg.de>
2024-06-10 11:51:09 +0200

    Add Feature
    scale x and y values

--- main.py
+++ main.py
@@ -21,12 +21,15 @@ p0 = [A_0, B_0]
 
 # Configuration
 file_name = "FrequencySweep"
+title = None
 y_name = "Amplitude in mV"
 x_name = "height difference in m"
 
 split_sign = ";"
 x_column = 0
 y_column = 1
+x_scale = 1
+y_scale = 1
 
 fit_start = 6.78e-7
 fit_end = None
@@ -36,6 +39,6 @@ y_log = False
 
 
 popt, error = plot_file(file_name, split_sign=split_sign, y_name=y_name, x_name=x_name, x_log=x_log, y_log=y_log,
-                        x_column=x_column, y_column=y_column,
+                        x_column=x_column, y_column=y_column, x_scale=x_scale, y_scale=y_scale,
                         model_function=model_function, p0=p0, fit_start=fit_start, fit_end=fit_end)
 print(f"Optimal parameters {p_str}: {popt}, error: {error}")
--- modules/PlotTxtFile.py
+++ modules/PlotTxtFile.py
@@ -16,6 +16,7 @@ fitting (see example in main.py):
 
 
 def plot_file(saved_file_name, split_sign=";", y_name="y", x_name="x", x_column=0, y_column=1,
+              x_scale=1.0, y_scale=1.0,
               model_function=None, p0=None, fit_start=None, fit_end=None, y_log=False, x_log=False):
     error = None
     popt = None
@@ -25,10 +26,10 @@ def plot_file(saved_file_name, split_sign=";", y_name="y", x_name="x", x_column=
     with open(plot_data, 'r') as file:
         for line in file:
             data_line = line.strip().split(split_sign)
-            x_value = data_line[x_column]
-            y_value = data_line[y_column]
-            x_list.append(float(x_value))
-            y_list.append(float(y_value))
+            x_value = float(data_line[x_column]) * x_scale
+            y_value = float(data_line[y_column]) * y_scale
+            x_list.append(x_value)
+            y_list.append(y_value)
 
     x_data = np.array(x_list)
     y_data = np.array(y_list)
