374c503c6ad54aefcc67001a0d7db56b7032f168
Niklas Bliemel <niklas.bliemel@stud.uni-regensburg.de>
2024-06-12 08:37:21 +0200

    clean code

--- main.py
+++ main.py
@@ -24,7 +24,10 @@ x_log = False
 y_log = False
 
 
-# Configure fit (comment if not needed)
+# Configure fit (True/False)
+use_fit = True
+
+
 def model_function(x, A, B):
     return A * x + B
 
@@ -33,18 +36,17 @@ def model_function(x, A, B):
 A_0 = -5
 B_0 = 0
 p0 = [A_0, B_0]
-p_str = "A, B"
+para_str = "A, B"
 
+# Fit Range
 fit_start = 6.78e2
 fit_end = None
 
 # ---------------------------
 
 
-if "model_function" not in locals():
+if not use_fit:
     model_function = None
-
-if "p0" not in locals():
     p0 = None
 
 opt_paras, errors = plot_file(file_name, title=title, split_sign=split_sign, x_name=x_name, y_name=y_name, x_log=x_log,
@@ -52,4 +54,4 @@ opt_paras, errors = plot_file(file_name, title=title, split_sign=split_sign, x_n
                               model_function=model_function, p0=p0, fit_start=fit_start, fit_end=fit_end)
 
 if opt_paras is not None and errors is not None:
-    print(f"Optimal parameters {p_str}: {popt}, error: {error}")
+    print(f"Optimal parameters {para_str}: {opt_paras}, error: {errors}")
